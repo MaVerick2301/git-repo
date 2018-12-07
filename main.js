8abb6f4b8c004a5a6fa092cf8ed7e079392fc204
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-07 18:01:53 +0300

    Lesson2

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
+var a = 4;
+var b = -5;
+if (a > 0 && b > 0) {
+alert(a - b);
+} else if (a < 0 && b < 0) {
+    alert(a * b);
+} else {
+    alert(a + b);
+}
 
 
-alert('Hello World');
-alert('TEST?!')
+
+
+a = Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+while (a <= 15) {
+    alert(a);
+    ++a;
+}
+
+function addition (x, y) {
+    return x + y;
+}
+
+function substraction (x, y) {
+    return x - y;
+} 
+
+function multiplication (x, y) {
+    return x * y;
+}
+
+function division (x, y) {
+    return x / y;
+}
+
+function mathOperation (arg1, arg2, operation) {
+    var arg1 = x;
+    var arg2 = y;
+    var operation;
+     
