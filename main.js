6bbb0866ba8da17b01c11c451651ce9276dfab2f
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-13 18:31:10 +0300

    заготовка цикла для разбора товара и цены

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
-
-
+for (var i = 0; i < productsArr.length; i++) {
+    var k = (myGarr[i].indexOf(':'));
+    govno.push(myGarr[i].slice(0, k));
+    console.log(govno)
+    }
 
 
 
@@ -12,16 +15,14 @@ while (a <= 100) {
 }
 
 
-
-
-
-
 for (var i = 1; i < 10; i++)
 console.log(i);
 
 var i = 1;
 var n = 20;
 var k = 'x';
+
+
 while(true) {
     console.log(k);
     k = k + 'x';
