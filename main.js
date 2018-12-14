e09d60c2723d9b891935433b1502cd51291458a8
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-14 14:11:27 +0300

    Додедал задание 3 урока

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 
-for (var i = 0; i < productsArr.length; i++) {
-    var k = (myGarr[i].indexOf(':'));
-    govno.push(myGarr[i].slice(0, k));
-    console.log(govno)
+//____________   Задание №2 "Корзина" 
+
+
+var arrCart = ['T-shirt:20.37', 'Shoes:62.43', 'Jacket:146.95',
+    'Jeans:42.80']; //"Входной" необработанный массив
+var arrItems = []; //массив для товаров
+var arrPrice = []; //массив для цен
+var arrPriceWithDollar = []; //массив для вывода, с долларом
+var totalPrice = 0; //переменная для суммы покупок
+
+for (var i = 0; i < arrCart.length; i++) {
+    var k = (arrCart[i].indexOf(':')); //ловлю позицию разделителя
+    arrItems.push(arrCart[i].slice(0, k)); //собираю массив из товаров для показа, весь текст налево от разделителя
+    arrPrice.push(arrCart[i].slice(k+1)); //собираю "технический" масив для расчетов, направо от разделителя
+    arrPriceWithDollar.push(arrCart[i].slice(k+1) + '$'); //собираю массив из цен для показа, с добавлением доллара
     }
+for (var i = 0; i < arrPrice.length; i++) {
+    var arrString = arrPrice[i].toString(); //преобразую каждый элемент массива в отдельную строку
+    y = Number(arrString); //преобразую строку в число
+    totalPrice = totalPrice + y; //считаю общую сумму покупок, обрабатывая поочередно элементы массива
+}
+totalPrice = totalPrice.toFixed(); //привожу общую сумму к целому
+alert('Your cart is: ' + arrItems + ' with this prices: ' + 
+arrPriceWithDollar  + ' and the total price is ' + totalPrice + '$' ); //окончательный вывод
 
+   
 
+//____________   Задание №1 "С помощью цикла while вывести все простые числа в промежутке от 0 до 100."
 
 
 var a = 1;
@@ -14,14 +35,19 @@ while (a <= 100) {
     ++a;
 }
 
+//____________   Задание №3 "Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла."
+
 
 for (var i = 1; i < 10; i++)
 console.log(i);
 
+
+//____________   Задание №4 "Нарисовать пирамиду с 20 рядами с помощью console.log"
+
+
 var i = 1;
 var n = 20;
 var k = 'x';
-
 
 while(true) {
     console.log(k);
