b5122cf6d29275562cf332bead19e4f1809e63ab
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-20 18:49:51 +0300

    Дполонения к 5 ДЗ

--- chess.js
+++ chess.js
@@ -9,7 +9,8 @@ var $divTest3 = document.createElement('div');
 $divTest2.classList.add('testJsDiv2');
 $divTest3.classList.add('testJsDiv2');
 
-for (var i = 0; i < 64; i= i + 1 ) {
+
+for (var i = 0; i < 8; i= i + 1 ) {
     var $divTest4 = document.createElement('div');
     $divTest.appendChild($divTest4);
     if (i % 2 !== 0) {
