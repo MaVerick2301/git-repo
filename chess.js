8fd7f642e2122eeabcc32ef774c0d5ff002e54b1
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-20 18:20:23 +0300

    Подготовка к 1 заданию

--- chess.js
+++ chess.js
@@ -1,38 +1,21 @@
 
-var benchTable;
-
-
-
-benchTable = document.getElementById('bench-table');
-while (benchTable.firstChild) {
-  benchTable.removeChild(benchTable.firstChild);
-}
-
-
-var tbody = document.createElement('TBODY');
-
-for (var i = 0; i < 9; i++) {
-  var tr = document.createElement('TR');
-  tbody.appendChild(tr);
-  for (var j = 0; j < 9; j++) {
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.appendChild(document.createTextNode('' + i.toString(9) + j.toString(9)));
-  }
-}
-
-benchTable.appendChild(tbody);
 
 
 
 
-var $divTest = document.getElementsByClassName('testJsDiv');
+var $divTest = document.getElementById('testJsDivId');
 var $divTest2 = document.createElement('div');
+var $divTest3 = document.createElement('div');
 $divTest2.classList.add('testJsDiv2');
+$divTest3.classList.add('testJsDiv2');
 
-
-
-while (i < 81) {
-$divTest[0].appendChild($divTest2);
-i++;
+for (var i = 0; i < 64; i= i + 1 ) {
+    var $divTest4 = document.createElement('div');
+    $divTest.appendChild($divTest4);
+    if (i % 2 !== 0) {
+        $divTest4.classList.add('testJsDiv2');
+    } else {
+        $divTest4.classList.add('testJsDiv3');
+    }
 }
+
