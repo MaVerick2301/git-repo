99a6acd55f73e2c2207893085a26c6159608a164
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-21 15:55:29 +0300

    Переделал 5 ДЗ

--- chess.js
+++ chess.js
@@ -1,22 +1,67 @@
 
 
+function generateChessTable () {
 
 
 
-var $divTest = document.getElementById('testJsDivId');
-var $divTest2 = document.createElement('div');
-var $divTest3 = document.createElement('div');
-$divTest2.classList.add('testJsDiv2');
-$divTest3.classList.add('testJsDiv2');
+var $chessBody = document.createElement('div');
+var $chessHeader = document.createElement('div');
+var $chessContainer = document.createElement('div');
+var $chessMarkersLeft = document.createElement('div');
+var $chessTable = document.createElement('div');
+
+$chessBody.classList.add('chessBody');
+$chessHeader.classList.add('chessHeader');
+$chessContainer.classList.add('chessContainer');
+$chessMarkersLeft.classList.add('chessMarkersLeft');
+$chessTable.classList.add('chessTable');
 
 
-for (var i = 0; i < 8; i= i + 1 ) {
-    var $divTest4 = document.createElement('div');
-    $divTest.appendChild($divTest4);
+document.body.appendChild($chessBody);
+$chessBody.appendChild($chessHeader);
+$chessBody.appendChild($chessContainer);
+$chessContainer.appendChild($chessMarkersLeft);
+$chessContainer.appendChild($chessTable);
+
+
+
+
+for (i = 0; i < 8; i++) {  
+    var $chessTableString = document.createElement('div');
+    
     if (i % 2 !== 0) {
-        $divTest4.classList.add('testJsDiv2');
+      $chessTableString.classList.add('chessTableEvenString');
+      $chessTable.appendChild($chessTableString);
+      for (j = 0; j < 8; j++) {
+        var $chessTableSquare = document.createElement('div');
+        
+        if (j % 2 == 0) {
+          $chessTableSquare.classList.add('chessTableSquareWhite');
+          $chessTableString.appendChild($chessTableSquare);
+        } else {
+          $chessTableSquare.classList.add('chessTableSquareBlack');
+          $chessTableString.appendChild($chessTableSquare); 
+        }
+      } 
+        
     } else {
-        $divTest4.classList.add('testJsDiv3');
+      $chessTableString.classList.add('chessTableOddString');
+      $chessTable.appendChild($chessTableString);
+      for (j = 0; j < 8; j++) {
+        var $chessTableSquare = document.createElement('div');
+        
+        if (j % 2 == 0) {
+          $chessTableSquare.classList.add('chessTableSquareWhite');
+          $chessTableString.appendChild($chessTableSquare);
+        } else {
+          $chessTableSquare.classList.add('chessTableSquareBlack');
+          $chessTableString.appendChild($chessTableSquare); 
+        }
+      }
     }
 }
+
+
+}
+
 
