fdf72dc163fccfbbaf278122e13788432f7a5376
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-21 10:36:34 +0300

    Удалил ненужное

--- chess.js
+++ chess.js
@@ -1,29 +1,7 @@
 
 
 
-var benchTable;
 
-
-
-benchTable = document.getElementById('bench-table');
-while (benchTable.firstChild) {
-  benchTable.removeChild(benchTable.firstChild);
-}
-
-
-var tbody = document.createElement('TBODY');
-
-for (var i = 0; i < 9; i++) {
-  var tr = document.createElement('TR');
-  tbody.appendChild(tr);
-  for (var j = 0; j < 9; j++) {
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.appendChild(document.createTextNode('' + i.toString(9) + j.toString(9)));
-  }
-}
-
-benchTable.appendChild(tbody);
 
 
 
