88298f86f82af3a6ba1e110075afb15147ed56ac
Vlad Kolosov <vladkolosov.l@gmail.com>
2018-12-21 17:33:25 +0300

    немного дополнил 5 ДЗ

--- chess.js
+++ chess.js
@@ -4,64 +4,90 @@ function generateChessTable () {
 
 
 
-var $chessBody = document.createElement('div');
-var $chessHeader = document.createElement('div');
-var $chessContainer = document.createElement('div');
-var $chessMarkersLeft = document.createElement('div');
-var $chessTable = document.createElement('div');
+    var $chessBody = document.createElement('div');
+    var $chessHeader = document.createElement('div');
+    var $chessContainer = document.createElement('div');
+    var $chessMarkersLeft = document.createElement('div');
+    var $chessTable = document.createElement('div');
 
-$chessBody.classList.add('chessBody');
-$chessHeader.classList.add('chessHeader');
-$chessContainer.classList.add('chessContainer');
-$chessMarkersLeft.classList.add('chessMarkersLeft');
-$chessTable.classList.add('chessTable');
-
-
-document.body.appendChild($chessBody);
-$chessBody.appendChild($chessHeader);
-$chessBody.appendChild($chessContainer);
-$chessContainer.appendChild($chessMarkersLeft);
-$chessContainer.appendChild($chessTable);
+    $chessBody.classList.add('chessBody');
+    $chessHeader.classList.add('chessHeader');
+    $chessContainer.classList.add('chessContainer');
+    $chessMarkersLeft.classList.add('chessMarkersLeft');
+    $chessTable.classList.add('chessTable');
 
 
+    document.body.appendChild($chessBody);
+    $chessBody.appendChild($chessHeader);
+    $chessBody.appendChild($chessContainer);
+    $chessContainer.appendChild($chessMarkersLeft);
+    $chessContainer.appendChild($chessTable);
 
 
-for (i = 0; i < 8; i++) {  
-    var $chessTableString = document.createElement('div');
-    
-    if (i % 2 !== 0) {
-      $chessTableString.classList.add('chessTableEvenString');
-      $chessTable.appendChild($chessTableString);
-      for (j = 0; j < 8; j++) {
-        var $chessTableSquare = document.createElement('div');
+
+
+
+
+
+    for (i = 0; i < 8; i++) {  
+        var $chessTableString = document.createElement('div');
         
-        if (j % 2 == 0) {
-          $chessTableSquare.classList.add('chessTableSquareWhite');
-          $chessTableString.appendChild($chessTableSquare);
+        if (i % 2 !== 0) {
+        $chessTableString.classList.add('chessTableEvenString');
+        $chessTable.appendChild($chessTableString);
+        for (j = 0; j < 8; j++) {
+            var $chessTableSquare = document.createElement('div');
+            
+            if (j % 2 == 0) {
+            $chessTableSquare.classList.add('chessTableSquareWhite');
+            $chessTableString.appendChild($chessTableSquare);
+            } else {
+            $chessTableSquare.classList.add('chessTableSquareBlack');
+            $chessTableString.appendChild($chessTableSquare); 
+            }
+        } 
+            
         } else {
-          $chessTableSquare.classList.add('chessTableSquareBlack');
-          $chessTableString.appendChild($chessTableSquare); 
+        $chessTableString.classList.add('chessTableOddString');
+        $chessTable.appendChild($chessTableString);
+        for (j = 0; j < 8; j++) {
+            $chessTableSquare = document.createElement('div');
+            
+            if (j % 2 == 0) {
+            $chessTableSquare.classList.add('chessTableSquareWhite');
+            $chessTableString.appendChild($chessTableSquare);
+            } else {
+            $chessTableSquare.classList.add('chessTableSquareBlack');
+            $chessTableString.appendChild($chessTableSquare);
+            }
         }
-      } 
-        
-    } else {
-      $chessTableString.classList.add('chessTableOddString');
-      $chessTable.appendChild($chessTableString);
-      for (j = 0; j < 8; j++) {
-        $chessTableSquare = document.createElement('div');
-        
-        if (j % 2 == 0) {
-          $chessTableSquare.classList.add('chessTableSquareWhite');
-          $chessTableString.appendChild($chessTableSquare);
-        } else {
-          $chessTableSquare.classList.add('chessTableSquareBlack');
-          $chessTableString.appendChild($chessTableSquare); 
         }
-      }
     }
 }
 
+function generateShoppingCart() {
+
+    var tShirtItemObj = {price: 20.37, quantity: 3};
+    var shoesItemObj = {price: 62.43, quantity: 1};
+    var jacketItemObj = {price: 146.95, quantity: 1};
+    var jeansItemObj = {price: 42.80, quantity: 2};
+    var totalPrice = 0;
+    var totalQuantity = 0;
+    var myCartArr = [
+        tShirtItemObj,
+        jacketItemObj,
+        shoesItemObj,
+        jeansItemObj
+    ];
+
+    for (var i in myCartArr) {
+        totalPrice = totalPrice + myCartArr[i].price * myCartArr[i].quantity;
+        totalQuantity = totalQuantity + myCartArr[i].quantity;
+
+    }
+
+
+    $shoppingCart = document.getElementById('shoppingCart');
+    $shoppingCart.innerHTML = 'Товаров в корзине: ' + totalQuantity + ',' + ' Общая стоимость покупок: ' + totalPrice.toFixed() + '$';
 
 }
-
-
--- main.js
+++ main.js
@@ -4,24 +4,24 @@
 function getObject(myInputNumber) {
 
 if (myInputNumber <= 999) {
-    var myInputString = myInputNumber.toString();
-    var myOutputObj;
-    if (myInputString.length === 3) {
-    myOutputObj = {
-     units: myInputString[2],
-     tens: myInputString[1],
-     hundreds: myInputString[0],
+  var myInputString = myInputNumber.toString();
+  var myOutputObj;
+  if (myInputString.length === 3) {
+  myOutputObj = {
+      units: myInputString[2],
+      tens: myInputString[1],
+      hundreds: myInputString[0],
+  };
+  } else if (myInputString.length === 2) {
+      myOutputObj = {
+      tens: myInputString[0],
+      units: myInputString[1],
     };
-    } else if (myInputString.length === 2) {
-        myOutputObj = {
-            tens: myInputString[0],
-            units: myInputString[1],
-        };
-    } else {
-        myOutputObj = {
-            units: myInputString[0]
-        };
-    }
+  } else {
+      myOutputObj = {
+          units: myInputString[0]
+    };
+  }
     console.log(myOutputObj);
 } else {
     console.log('Error: Input  > 999 OR Input is NaN');
@@ -38,14 +38,14 @@ var jacketItemObj = {price: 146.95, quantity: 1};
 var jeansItemObj = {price: 42.80, quantity: 2};
 var totalPrice = 0;
 var myCartObj = {
-    tShirtItemObj,
-    jacketItemObj,
-    shoesItemObj,
-    jeansItemObj
+  tShirtItemObj,
+  jacketItemObj,
+  shoesItemObj,
+  jeansItemObj
 };
 
 for (var i in myCartObj) {
-    totalPrice = totalPrice + myCartObj[i].price * myCartObj[i].quantity;
+  totalPrice = totalPrice + myCartObj[i].price * myCartObj[i].quantity;
 }
 console.log('Общая стоимость покупок: ' + totalPrice.toFixed() + '$');
 
